50b867e175b580296c90288d435af8bb374a76f0
MickaelRigault <mick.rigault@gmail.com>
2021-06-07 14:48:10 +0200

    advancing dask

--- ztfimg/dask.py
+++ ztfimg/dask.py
@@ -7,12 +7,13 @@ from ztfquery import io
 
 
 class DaskScienceFiles( object ):
-    """ """
+
+    _TO_STORED = ["rcid"]
     def __init__(self, filenames, **kwargs):
         """ """
         self.set_filenames(filenames, **kwargs)
         
-    def set_filenames(self, filenames, npartitions=None, chunksize=10, persist=False):
+    def set_filenames(self, filenames, npartitions=None, chunksize=10, persist=False, suffix=None, **kwargs):
         """ """
         # 
         ddfile = dd.from_pandas(pandas.DataFrame(filenames, columns=["filename"]),
@@ -21,16 +22,43 @@ class DaskScienceFiles( object ):
         # compute head for structuring
         dhead = ddfile.head(n=1)["filename"].apply(io.parse_filename)
         dbase = ddfile["filename"].apply(io.parse_filename, meta=dhead, as_serie=True)
-        dbase["filename"] = ddfile["filename"]
+        if suffix is None:
+            dbase["filename"] = ddfile["filename"]
+        else:
+            dbase["filename"]  = ddfile["filename"].apply( io.get_file, suffix=suffix, **kwargs)
         
         self._datafile = dbase
         if persist:
             self.persist()
-            
+
+        self.reset()
+
     def persist(self):
         """ """
         self._datafile = self._datafile.persist()
 
+    def reset(self):
+        """ """
+        for k in _TO_STORED:
+            setattr(self,f"_{k}",None)
+        
+
+    # ------- #
+    #  GETTER #
+    # ------- #
+    def get_rcid(self, recompute=False):
+        """ use self.rcid """
+        if recompute:
+            return self.datafile.groupby("rcid").size().compute()
+        
+        return self.rcid
+
+    def get_rcid_data(self, rcid):
+        """ """
+        return self.datafile.query(f"rcid == '{rcid}")
+
+    def get_radec()
+    
     # ================= #
     #   Properties      #
     # ================= #
@@ -38,3 +66,10 @@ class DaskScienceFiles( object ):
     def datafile(self):
         """ """
         return self._datafile
+
+    @property
+    def rcid(self):
+        """ """
+        if not hasattr(self,"_rcid") or self._rcid is None:
+            self._rcid = self.get_rcid(recompute=True)
+        return self._rcid
