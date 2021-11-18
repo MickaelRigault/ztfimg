fec76f75a550c7a07040eae577cf6fdd635e8168
MickaelRigault <mick.rigault@gmail.com>
2021-11-18 12:26:17 +0100

    test_

--- ztfimg/raw.py
+++ ztfimg/raw.py
@@ -67,7 +67,7 @@ class RawQuadrant( _Quadrant_ ):
             header    = fits.getheader(filename, qid=qid)
 
         if qid in [2, 3]:
-            overscan = overscan[:,::-1]
+            #overscan = overscan[:,::-1]
 
         if persist and use_dask:
             data = data.persist()
