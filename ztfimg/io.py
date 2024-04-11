1eb25f118d833cd43097e86bd317ca8d63095ffb
Simon Conseil <s.conseil@ip2i.in2p3.fr>
2024-04-11 17:28:36 +0200

    fix invalid escape sequence

--- ztfimg/io.py
+++ ztfimg/io.py
@@ -15,8 +15,8 @@ def get_test_image():
     return sciimg, maskimg
 
 def get_nonlinearity_table():
-    """ get the nonlinearity table 
-    
+    """ get the nonlinearity table
+
     Returns
     -------
     DataFrame
@@ -24,7 +24,7 @@ def get_nonlinearity_table():
     from .utils.tools import ccdid_qid_to_rcid
 
     NONLINEARITY_FILE = os.path.join(PACKAGE_PATH, "data/ccd_amp_coeff_v2.txt")
-    nl_table = pandas.read_csv(NONLINEARITY_FILE, comment='#', header=None, sep='\s+', usecols=[0, 1, 2, 3, 4],
+    nl_table = pandas.read_csv(NONLINEARITY_FILE, comment='#', header=None, sep=r'\s+', usecols=[0, 1, 2, 3, 4],
                                       names=["ccdid", "ampname", "qid", "a", "b"])
     nl_table["qid"] += 1 # qid in the file is actually AMP_ID that starts at 0, while qid starts at 1.
     nl_table["rcid"] = ccdid_qid_to_rcid(nl_table["ccdid"].astype("int"), nl_table["qid"].astype("int"))
