be25fde4cc1b838012ff826d3944da2a2f7760fc
MickaelRigault <mick.rigault@gmail.com>
2026-04-14 15:32:38 +0200

    testing bugfix

--- ztfimg/__init__.py
+++ ztfimg/__init__.py
@@ -1,5 +1,5 @@
 from ._version import __version__  # noqa
 from .base import *  # noqa
 from .science import *  # noqa
-from .raw import *  # noqa
+from .raw import RawQuadrant, RawCCD, RawFocalPlane  # noqa
 from .utils.tools import *  # noqa
